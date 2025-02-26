16397427411540ffce3cbcb856c44a3058f25528
AstridK14 <ap.kiaraastrid.magpantay@apecschools.edu.ph>
2025-02-26 07:58:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchform.classList.toggle('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+let loginform = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginform.classList.toggle('active');
+    searchform.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+
+
+/*let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onlick = () =>
+{
+    shoppingCart.classList.toggle('active');
+}*/
+
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    loginform.classList.remove('active');
+}
+
+
+
+
+
+window.onscroll = () =>
+{
+    searchform.classList.remove('active');
+    loginform.classList.remove('active');
+    navbar.classList.remove('active');
+}
